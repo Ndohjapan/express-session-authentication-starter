243c95b85bbacbe917b9837c55ec2ec133c9c382
Joel <ndohjoelmbj16@gmail.com>
2023-06-03 12:19:59 +0100

    simple express session

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
 const MongoStore = require('connect-mongo')(session);
 
 // Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
+// require('./config/passport');
 
 /**
  * -------------- GENERAL SETUP ----------------
@@ -22,35 +18,35 @@ require('dotenv').config();
 // Create the Express application
 var app = express();
 
+const dbString = 'mongodb://localhost:27017/user_authentication'
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const connection = mongoose.createConnection(dbString, dbOptions);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const sessionStore = new MongoStore({
+    mongooseConnection: connection,
+    collection: "sessions"
+});
 
-/**
- * -------------- SESSION SETUP ----------------
- */
-
-// TODO
-
-/**
- * -------------- PASSPORT AUTHENTICATION ----------------
- */
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-/**
- * -------------- ROUTES ----------------
- */
-
-// Imports all of the routes from ./routes/index.js
-app.use(routes);
+app.use(session({
+    secret: "some secret",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 
 
-/**
- * -------------- SERVER ----------------
- */
+app.get("/", (req, res, next) => {
+    res.send('<h1>Hello World (Sessions)</h1>')
+})
 
-// Server listens on http://localhost:3000
-app.listen(3000);
+app.listen(3000)
